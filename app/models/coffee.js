c06877b4c0a8e1ab6f3205851f126ccb93b8dad6
Cory Westerfield <cory.westerfield@gmail.com>
2014-12-22 21:54:10 -0700

    Added fixtures and models for data

--- app/models/coffee.js
+++ app/models/coffee.js
@@ -1,9 +1,40 @@
 import DS from "ember-data";
 
-App.Coffee = DS.Model.extend({
+var Coffee = DS.Model.extend({
+  name:       DS.attr('string'),
   location:   DS.attr('string'),
   type:       DS.attr('string'),
   price:      DS.attr('number')
 });
 
-export default DS.Model.extend({});
+
+Coffee.reopenClass({
+  FIXTURES: [
+  {
+    id: 1,
+    name: 'Nova',
+    location: 'Denver',
+    type: 'Dark',
+    price: 10},
+  {
+    id: 2,
+    name: 'Black Eye',
+    location: 'Denver',
+    type: 'Light',
+    price: 5},
+  {
+    id: 3,
+    name: 'Starbucks',
+    location: 'Indianapolis',
+    type: 'Medium',
+    price: 7},
+  {
+    id: 4,
+    name: 'Dogwood',
+    location: 'Minneapolis',
+    type: 'Dark',
+    price: 12}
+  ]
+});
+
+export default Coffee;
--- app/routes/coffee.js
+++ app/routes/coffee.js
@@ -1,3 +1,9 @@
 import Ember from "ember";
 
-export default Ember.Route.extend({});
+var CoffeeRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('coffee');
+  }
+});
+
+export default CoffeeRoute;
